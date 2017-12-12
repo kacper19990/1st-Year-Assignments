8c0a55aeafcdb43757cd1dd21bce0ebe90307f95
Kacper <kacper19990@hotmail.com>
2017-12-12 20:19:29 +0000

    Fix #1

--- High Scores/src/HighScores.java
+++ High Scores/src/HighScores.java
@@ -1,16 +1,14 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class HighScores
 {
-    public static int[] initialiseHighScores(int[] highScores)
+    public static void initialiseHighScores(int[] highScores)
     {
         Arrays.fill(highScores, 0);
-        return highScores;
     }
 
-    public static String printHighScores(int[] scores)
+    public static void printHighScores(int[] scores)
     {
         int i = 0;
         System.out.println("The high scores are");
@@ -22,29 +20,21 @@ public class HighScores
             System.out.print(scores[i]);
             i++;
         }while (i < scores.length);
-        return null;
     }
 
     public static boolean higherThan(int[] scores, int newScore)
     {
-        boolean truth = false;
-        int i = 0;
-        do {
-            if(newScore > scores[i])
-                truth = true;
-            else if(i < scores.length)
-                i++;
-            else
-                truth = false;
-        }while (i < scores.length);
-        return(truth);
+        System.out.println("Error8");
+        return(newScore > scores[scores.length -1]);
+
+
     }
     public static void insertScore(int[] scores, int newScore)
     {
         while (scores != null)
         {
             int[] tempArray = new int[(scores.length + 1)];
-            System.arraycopy(scores, 0, tempArray, 0, (scores.length + 1));
+            System.arraycopy(scores, 0, tempArray, 0, (scores.length));
             if (higherThan(scores, newScore))
             {
                 int i = 0;
@@ -61,41 +51,28 @@ public class HighScores
                     }
                 }while (i < scores.length);
             }
-            else
-            {
-                tempArray[scores.length] = newScore;
-                scores = tempArray;
-            }
         }
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("How many high scored do you want to add?");
         int number = input.nextInt();
-        System.out.println("Insert the high scores with ',' in between each value");
-        int i = 0;
-        int[] highScores = {};
-        do {
-            int score = input.nextInt();
-            highScores[i] = score;
-            i++;
-        }while(i < number);
+        int[] highScores = new int[number];
         initialiseHighScores(highScores);
-        boolean loop = true;
+        boolean finished = false;
         do {
-            System.out.println("Input the next high score.");
-            input = new Scanner(System.in);
-            if(input.hasNextInt())
+            System.out.println("Insert the next score.");
+            int score = input.nextInt();
+            if(higherThan(highScores, score))
             {
-                int score = input.nextInt();
                 insertScore(highScores, score);
-
+                printHighScores(highScores);
             }
             else
-                loop = false;
-        }while(loop);
-        printHighScores(highScores);
+            {
+                System.out.println("Invalid input. Try again? Y/N");
+                if(input.hasNext("N"))
+            }
+        }while(!finished);
     }
-
 }

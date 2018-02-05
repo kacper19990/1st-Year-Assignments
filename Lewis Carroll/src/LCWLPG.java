8a16dcc18545722a3a29cfb7a900157f2e0e61a5
Kacper <kacper19990@hotmail.com>
2018-02-05 22:55:30 +0000

    80% completion of assignment for Tuesday.

--- Lewis Carroll/src/LCWLPG.java
+++ Lewis Carroll/src/LCWLPG.java
@@ -24,43 +24,57 @@ public class LCWLPG {
         return words;
     }
     static List<String> readWordList(String input){
+        System.out.println("Word list read.");
         return Arrays.asList(input.split("\\s*,\\s*"));
     }
 
     static boolean isUniqueList(List<String> inputList){
         Set<String> set = new HashSet<>(inputList);
+        System.out.println("Reached isUniqueList.");
         return set.size() >= inputList.size();
 
     }
 
     static boolean isEnglishWord(String input, List<String> dictionary){
         Set<String> set = new HashSet<>(dictionary);
+        System.out.println("isEnglishWord reached");
         return set.contains(input);
     }
 
     static boolean isDifferentByOne(String input1, String input2){
-        String[] array1 = input1.split("");
-        String[] array2 = input2.split("");
+        char[] array1 = input1.toCharArray();
+        char[] array2 = input2.toCharArray();
+        int differentChars;
 
-        Set<String> set1 = new HashSet<>(Arrays.asList(array1));
-        Set<String> set2 = new HashSet<>(Arrays.asList(array2));
+        if(array1.length == array2.length){
+            return false;
+        }
+        else{
+            for(int i = 0; i < array1.length; i++){
+                if(i !=array1.length-1){
 
-        return set1 == set2;
+                }
+            }
+        }
+
     }
 
-    static boolean isWordChain(List<String> list){
+    static boolean isWordChain(List<String> list, String fileLocation){
         boolean determinant = false;
         if(isUniqueList(list)) {
-            for (int i = 0; i < list.size(); i++) {
-                if (isDifferentByOne(list.get(i), list.get(i+1))){
-                    if(isEnglishWord(list.get(i), list)){
+            System.out.println("Is Unique.");
+            for (int i = 1; i < list.size(); i++) {
+                if (isDifferentByOne(list.get(i-1), list.get(i))){
+                    if(isEnglishWord(list.get(i), readDictionary())){
                         determinant = true;
                     }
                     else {
+                        System.out.println("not english");
                         determinant = false;
                         break;
                     }
                 }
+                else System.out.println("not different by one");
             }
         }
         return determinant;
@@ -69,20 +83,21 @@ public class LCWLPG {
     public static void main(String[] args){
         boolean finished = false;
         Scanner scanner = new Scanner(System.in);
-        try {
-            readDictionary("C:\\Users\\kacpe\\Desktop\\words.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         do{
             System.out.println("Enter a comma separated list of words (or an empty list to quit)");
             String readString = scanner.nextLine();
             if(readString.isEmpty()){
                 finished = true;
-                System.out.println("exiting");
+                System.out.println("Exiting.");
             }
             else{
-                boolean test = isWordChain(readWordList(readString));
+                //System.out.println(readString);
+                if(isWordChain(readWordList(readString), "C:\\Users\\kacpe\\Desktop\\words.txt" )){
+                    System.out.println("Valid chain of words from Lewis Carroll's word-links game.");
+                }
+                else{
+                    System.out.println("Not a valid chain of words from Lewis Carroll's word-links game.");
+                }
 
             }
 

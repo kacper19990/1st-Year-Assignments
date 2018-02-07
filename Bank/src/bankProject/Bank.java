70ef4652b798a0d5daa549480b0c2cac30059d6f
Kacper <kacper19990@hotmail.com>
2018-02-07 14:27:49 +0000

    Newest Bank project

--- Bank/src/bankProject/Bank.java
+++ Bank/src/bankProject/Bank.java
@@ -1,88 +1,10 @@
 package bankProject;
 
-import jdk.nashorn.api.tree.Tree;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-public class Bank implements Bankinterface{
-    long account = 10001;
-
-    //static BankCustomer[] customerArray = new BankCustomer[100];
-    //static List<BankCustomer> customerList = new ArrayList<BankCustomer>();
-    static Map<Long, BankCustomer> customerTree = new TreeMap<Long, BankCustomer>();
-    public static void main(String[] args){
-
-        Bank bank = new Bank();
-
-        BankCustomer bankCustomer = new BankCustomer();
-        bankCustomer.setAccountNumber(123456789);
-        bankCustomer.setSortCode(12425);
-        bankCustomer.setCustomerName("Joe");
-        bankCustomer.setCustomerEmail("joe@tcd.ie");
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            bankCustomer.setCustomerDateOfBirth(formatter.parse("08/02/1999"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        bankCustomer.setCustomerAddress("Trinity College Dublin");
-        //customerList.add(bankCustomer);
-        bank.customerTree.put(new Long(45256789), bankCustomer);
-        bank.findCustomer(new Long(45256789));
-    }
-
-    @Override
-    public long createAccountNumber() {
-        return 0;
-    }
-
-    @Override
-    public BankCustomer findCustomer(long accountNumber) {
-        BankCustomer customer = null;
-        boolean customerFound = false;
-        int counter = 0;
-        /*while(!customerFound && counter <customerList.size()){
-            customer = customerList.get(counter);
-            if (customer.getAccountNumber() == accountNumber){
-                customerFound = true;
-            }
-            counter++;
-        }
-        */
-        return customerTree.get(accountNumber);
-    }
-
-    @Override
-    public BankCustomer findCustomer(String name, Date dateOfBirth, String address) {
-        return null;
-    }
-
-    @Override
-    public boolean debitAccount(long accountNumber, double amountDebited) {
-        BankCustomer customer = findCustomer(accountNumber);
-        boolean accountDebited = false;
-        double balance = customer.getBalance();
-        if(balance >= amountDebited){
-            customer.setBalance(balance - amountDebited);
-            accountDebited = true;
-        }
-        else
-            accountDebited = false;
-        return accountDebited;
-    }
-
-    @Override
-    public boolean creditAccount(long accountNumber, double amountCredited) {
-        BankCustomer customer = findCustomer(accountNumber);
-        customer.setBalance((customer.getBalance()+ amountCredited));
-        return true;
-    }
-
-    @Override
-    public void printBalance(long accountNumber) {
-
+public class Bank
+{
+    public static void main(String[] args)
+    {
+        BankOperation bank = new BankOperation();
+        bank.start();
     }
 }
-//TODO Assignment noughts and crosses, 2D arrays
--- Bank/src/bankProject/BankCustomer.java
+++ Bank/src/bankProject/BankCustomer.java
@@ -1,66 +1,93 @@
 package bankProject;
 
+import java.util.TreeMap;
 import java.util.Date;
 
-public class BankCustomer {
-    int accountNumber;
-    int sortCode;
-    double balance;
-    String customerEmail;
-    String customerDateOfBirth;
-    String customerAddress;
-    String customerName;
-
-
-
-    public double getBalance() {
-        return balance;
+public class BankCustomer
+{
+    BankCustomer()
+    {
+        customerTransactions = new TreeMap<>();
     }
-    public void setBalance(double balance) {
-        this.balance = balance;
+
+    private long accountNumber;
+    private long sortCode;
+    private String customerName;
+    private String customerAddress;
+    private String customerEmail;
+    private Date customerDateOfBirth;
+    private double balance;
+    private TreeMap<Date,Transaction> customerTransactions;
+
+    public TreeMap<Date,Transaction> getCustomerTransactions() {
+        return customerTransactions;
+    }
+
+    public void setCustomerTransactions(TreeMap<Date,Transaction> customerTransactions) {
+        this.customerTransactions = customerTransactions;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.customerTransactions.put(transaction.getTransactionDate(), transaction);
+    }
+
+    public Transaction getLastTransaction() {
+        return this.customerTransactions.get(customerTransactions.lastKey());
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public long getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(long sortCode) {
+        this.sortCode = sortCode;
     }
 
     public String getCustomerName() {
         return customerName;
     }
+
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public int getSortCode() {
-        return sortCode;
-    }
-    public void setSortCode(int sortCode) {
-        this.sortCode = sortCode;
-    }
-
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getCustomerDateOfBirth() {
-        return customerDateOfBirth;
-    }
-    public void setCustomerDateOfBirth(Date customerDateOfBirth) {
-        this.customerDateOfBirth = customerDateOfBirth;
     }
 
     public String getCustomerAddress() {
         return customerAddress;
     }
+
     public void setCustomerAddress(String customerAddress) {
         this.customerAddress = customerAddress;
     }
 
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
 
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Date getCustomerDateOfBirth() {
+        return customerDateOfBirth;
+    }
+
+    public void setCustomerDateOfBirth(Date customerDateOfBirth) {
+        this.customerDateOfBirth = customerDateOfBirth;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
